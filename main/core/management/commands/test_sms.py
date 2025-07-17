461df1e4b6b997670e18fd3c2ad343c182362249
Emre Özcan <67864140+eoezcan2@users.noreply.github.com>
2025-07-17 10:02:53 +0200

    Add client smpp test script

--- main/core/management/commands/test_sms.py
+++ main/core/management/commands/test_sms.py
@@ -2,28 +2,35 @@ from django.core.management.base import BaseCommand
 import smpplib.client
 import smpplib.consts
 
+def random_string(length=10):
+    import random
+    import string
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+
 class Command(BaseCommand):
-    help = "Test sending SMS via Jasmin"
+    help = "Simulate MO message via SMPP to Jasmin"
 
     def handle(self, *args, **kwargs):
-
         client = smpplib.client.Client('localhost', 2775)
+
+        # Optional: enable debug output
+        client.set_message_received_handler(lambda pdu: print("Received PDU:", pdu))
         client.connect()
         client.bind_transceiver(system_id='test', password='test')
 
-        client.send_message(
-            source_addr='491234567890',
-            destination_addr='Test',
-            short_message='This is an MO message'.encode('latin1'),
+        pdu = client.send_message(
+            source_addr_ton=1,
+            source_addr_npi=1,
+            source_addr=random_string(6),
 
-            source_addr_ton=1,  # SMPP_TON_INTL
-            source_addr_npi=1,  # SMPP_NPI_ISDN
+            dest_addr_ton=5,
+            dest_addr_npi=0,
+            destination_addr=random_string(6),
 
-            dest_addr_ton=5,    # SMPP_TON_ALNUM
-            dest_addr_npi=0,    # SMPP_NPI_UNKNOWN
-
+            short_message='This is an MT message'.encode('latin1'),
             data_coding=3,
-            esm_class=0
+            esm_class=0x00,
         )
 
+        self.stdout.write(self.style.SUCCESS(f"Message sent (sequence #{pdu.sequence})"))
         client.unbind()
